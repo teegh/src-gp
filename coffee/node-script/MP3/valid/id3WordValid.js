4575b26e692d4b63a4f9ca4b3e9b65e02d0a8570
test <test@test.jp>
2015-07-30 17:48:19 +0900

    valid

--- coffee/node-script/MP3/valid/id3WordValid.js
+++ coffee/node-script/MP3/valid/id3WordValid.js
@@ -17,40 +17,71 @@ var _ID3WordValid = (function(){  //jquery closure
     //年代の入力形式
     isYear : function (inStr){
       var retFlg = false;
+      var mes = "";
       if(inStr.match(/^[0-9][0-9][0-9][0-9]\/[0-9][0-9]\/[0-9][0-9]$/))retFlg = true;
       if(inStr.match(/^[0-9][0-9][0-9][0-9]$/))retFlg = true;
-      return retFlg;
+      if(!retFlg)mes = "「YYYY」または「YYYY/MM/DD」の形式になっていません。";
+      return mes;
     },
 
     //ファイル名とid3のタイトル名は同じ
     isSame_FileNameAndId3Titile : function (inFileName, inID3Title){
+      var retFlg = false;
+      var mes = "";
       if(inID3Title == inFileName.replace(/^[0-9][0-9] - /i,"")){
-        return true;
+        retFlg =  true;
       }else{
-        return false;
+        retFlg =  false;
       }
+      if(!retFlg)mes = "ファイル名とタイトルが一致しません。同じ内容にしてください。";
+      return mes;
     },
+
+    //ファイルのトラック番号
+    isTrackNo : function(inStr){
+      var retFlg = false;
+      var mes = "";
+      if(inStr.match(/^[0-9][0-9] - /g)){
+        retFlg = true;
+      }else{
+        retFlg = false;
+      }
+      if(!retFlg)mes = "トラック番号が含まれていないか、「NN - 」の形式となっていません。";
+      return mes;
+    },
+
+    //-------------------------------
 
     //空文字、null
     isNotNull : function(inStr){
+      var retFlg = false;
+      var mes = "";
       if(inStr == undefined || inStr == null || inStr == ""){
-        return false;
+        retFlg = false;
       }else{
-        return true;
+        retFlg = true;
       }
+      if(!retFlg)mes = "データが空です。文字を入力してください。";
+      return mes;
     },
 
     //前後のスペース
     isNotSpaceLR : function(inStr){
+      var retFlg = false;
+      var mes = "";
       if(inStr.match(/(^[ 　])|([ 　]$)/g)){
-        return false;
+        retFlg = false;
       }else{
-        return true;
+        retFlg = true;
       }
+      if(!retFlg)mes = "データの左端または右端に空白文字があります。空白は削除してください。";
+      return mes;
     },
 
     //特殊な空白
     isNotAnotherSpace : function(inStr){
+      var retFlg = false;
+      var mes = "";
       var regStr = "["+ String.fromCharCode(0x2002)
           + String.fromCharCode(0x2003)
           + String.fromCharCode(0x2004)
@@ -67,69 +98,96 @@ var _ID3WordValid = (function(){  //jquery closure
           + "]";
       var regex = new RegExp(regStr , "g");
       if(inStr.match(regex)){
-        return false;
+        retFlg = false;
       }else{
-        return true;
+        retFlg = true;
       }
+      if(!retFlg)mes = "特殊な空白があります。削除してください。";
+      return mes;
     },
 
     //一部の全角記号
     isNotZenkakuKigo : function(inStr){
-      if(inStr.match(/[＆‐―－’，．]/g)){
-        return false;
+      var retFlg = false;
+      var mes = "";
+      var matchArr = inStr.match(/[＆‐―－’，．]/g);
+      if(matchArr){
+        retFlg = false;
       }else{
-        return true;
+        retFlg = true;
       }
+      if(!retFlg)mes = "全角記号" + matchArr.toString() +"があります。半角記号に置き換えてください。";
+      return mes;
     },
 
     //一部の半角記号
     isNotHankakuKigo : function(inStr){
       //半角記号。チェック除外文字：',.&-
-      if(inStr.match(/[)(=･"!#$%\\*+\/:;<>?@[\]^_`{|}~｡｢｣､]/g)){
-        return false;
+      var retFlg = false;
+      var mes = "";
+      var matchArr = inStr.match(/[)(=･"!#$%\\*+\/:;<>?@[\]^_`{|}~｡｢｣､]/g);
+      if(matchArr){
+        retFlg = false;
       }else{
-        return true;
+        retFlg = true;
       }
+      if(!retFlg)mes = "半角記号"+matchArr.toString()+"があります。全角記号に置き換えてください。";
+      return mes;
     },
 
     //全角英数字
     isNotZenkaku : function(inStr){
-      if(inStr.match(/[１２３４５６７８９ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ]/g)){
-        return false;
+      var retFlg = false;
+      var mes = "";
+      var matchArr = inStr.match(/[１２３４５６７８９ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ]/g);
+      if(matchArr){
+        retFlg = false;
       }else{
-        return true;
+        retFlg = true;
       }
-    },
-
-    //ファイルのトラック番号
-    isTrackNo : function(inStr){
-      if(inStr.match(/^[0-9][0-9] - /g)){
-        return true;
-      }else{
-        return false;
-      }
+      if(!retFlg)mes = "全角英数字"+matchArr.toString()+"があります。半角英数字に置き換えてください。";
+      return mes;
     },
 
     //全角の3点リーダ
     isDotted : function(inStr){
+      var retFlg = false;
+      var mes = "";
       if(inStr.match(/…/g)){
-        return false;
+        retFlg = false;
       }else{
-        return true;
+        retFlg = true;
       }
+      if(!retFlg)mes = "…(3点リーダ)があります。半角文字のドット(.)3つに置き換えてください。";
+      return mes;
     },
 
     //機種依存文字
     isPlatformDependent : function(inStr){
       var retFlg = true;
+      var mes = "";
 
-      if(inStr.match(/[①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮⑯⑰⑱⑲⑳㍉㌔㌢㍍㌘㌧㌃㌶㍑㍗㌍㌦㌣㌫㍊㌻㎜㎝㎞㎎㎏㏄㎡㍻〝〟№㏍℡㊤㊥㊦㊧㊨㈱㈲㈹㍾㍽㍼≒≡∫∮∑√⊥∠∟⊿∵∩∪]/g))retFlg = false;
-      if(inStr.match(/[纊褜鍈銈蓜俉炻昱棈鋹曻彅丨仡仼伀伃伹佖侒侊侚侔俍偀倢俿倞偆偰偂傔僴僘兊兤冝冾凬刕劜劦勀勛匀匇匤卲厓厲叝﨎咜咊咩哿喆坙坥垬埈埇﨏塚增墲夋奓奛奝奣妤妺孖寀甯寘寬尞岦岺峵崧嵓﨑嵂嵭嶸嶹巐弡弴彧德忞恝悅悊惞惕愠惲愑愷愰憘戓抦揵摠撝擎敎昀昕昻昉昮昞昤晥晗晙]/g))retFlg = false;
-      if(inStr.match(/[晴晳暙暠暲暿曺朎朗杦枻桒柀栁桄棏﨓楨﨔榘槢樰橫橆橳橾櫢櫤毖氿汜沆汯泚洄涇浯涖涬淏淸淲淼渹湜渧渼溿澈澵濵瀅瀇瀨炅炫焏焄煜煆煇凞燁燾犱犾猤猪獷玽珉珖珣珒琇珵琦琪琩琮瑢璉璟甁畯皂皜皞皛皦益睆劯砡硎硤硺礰礼神祥禔福禛竑竧靖竫箞精絈絜綷綠緖繒罇羡羽茁荢荿菇]/g))retFlg = false;
-      if(inStr.match(/[菶葈蒴蕓蕙蕫﨟薰蘒﨡蠇裵訒訷詹誧誾諟諸諶譓譿賰賴贒赶﨣軏﨤逸遧郞都鄕鄧釚釗釞釭釮釤釥鈆鈐鈊鈺鉀鈼鉎鉙鉑鈹鉧銧鉷鉸鋧鋗鋙鋐﨧鋕鋠鋓錥錡鋻﨨錞鋿錝錂鍰鍗鎤鏆鏞鏸鐱鑅鑈閒隆﨩隝隯霳霻靃靍靏靑靕顗顥飯飼餧館馞驎髙髜魵魲鮏鮱鮻鰀鵰鵫鶴鸙黑ⅰⅱⅲⅳⅴⅵⅶⅷⅸ]/g))retFlg = false;
-      if(inStr.match(/[ⅹ￢￤＇＂￢￤＇＂㈱№㏍℡]/g))retFlg = false;
+      var matchArr1 = inStr.match(/[①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮⑯⑰⑱⑲⑳㍉㌔㌢㍍㌘㌧㌃㌶㍑㍗㌍㌦㌣㌫㍊㌻㎜㎝㎞㎎㎏㏄㎡㍻〝〟№㏍℡㊤㊥㊦㊧㊨㈱㈲㈹㍾㍽㍼≒≡∫∮∑√⊥∠∟⊿∵∩∪]/g);
+      var matchArr2 = inStr.match(/[纊褜鍈銈蓜俉炻昱棈鋹曻彅丨仡仼伀伃伹佖侒侊侚侔俍偀倢俿倞偆偰偂傔僴僘兊兤冝冾凬刕劜劦勀勛匀匇匤卲厓厲叝﨎咜咊咩哿喆坙坥垬埈埇﨏塚增墲夋奓奛奝奣妤妺孖寀甯寘寬尞岦岺峵崧嵓﨑嵂嵭嶸嶹巐弡弴彧德忞恝悅悊惞惕愠惲愑愷愰憘戓抦揵摠撝擎敎昀昕昻昉昮昞昤晥晗晙]/g);
+      var matchArr3 = inStr.match(/[晴晳暙暠暲暿曺朎朗杦枻桒柀栁桄棏﨓楨﨔榘槢樰橫橆橳橾櫢櫤毖氿汜沆汯泚洄涇浯涖涬淏淸淲淼渹湜渧渼溿澈澵濵瀅瀇瀨炅炫焏焄煜煆煇凞燁燾犱犾猤猪獷玽珉珖珣珒琇珵琦琪琩琮瑢璉璟甁畯皂皜皞皛皦益睆劯砡硎硤硺礰礼神祥禔福禛竑竧靖竫箞精絈絜綷綠緖繒罇羡羽茁荢荿菇]/g);
+      var matchArr4 = inStr.match(/[菶葈蒴蕓蕙蕫﨟薰蘒﨡蠇裵訒訷詹誧誾諟諸諶譓譿賰賴贒赶﨣軏﨤逸遧郞都鄕鄧釚釗釞釭釮釤釥鈆鈐鈊鈺鉀鈼鉎鉙鉑鈹鉧銧鉷鉸鋧鋗鋙鋐﨧鋕鋠鋓錥錡鋻﨨錞鋿錝錂鍰鍗鎤鏆鏞鏸鐱鑅鑈閒隆﨩隝隯霳霻靃靍靏靑靕顗顥飯飼餧館馞驎髙髜魵魲鮏鮱鮻鰀鵰鵫鶴鸙黑ⅰⅱⅲⅳⅴⅵⅶⅷⅸ]/g);
+      var matchArr5 = inStr.match(/[ⅹ￢￤＇＂￢￤＇＂㈱№㏍℡]/g);
 
-      return retFlg;
+      if(matchArr1)retFlg = false;
+      if(matchArr2)retFlg = false;
+      if(matchArr3)retFlg = false;
+      if(matchArr4)retFlg = false;
+      if(matchArr5)retFlg = false;
+
+      var matchmes = "";
+      if(matchArr1)matchmes += (matchmes != "" ? "," : "") + matchArr1.toString();
+      if(matchArr2)matchmes += (matchmes != "" ? "," : "") + matchArr2.toString();
+      if(matchArr3)matchmes += (matchmes != "" ? "," : "") + matchArr3.toString();
+      if(matchArr4)matchmes += (matchmes != "" ? "," : "") + matchArr4.toString();
+      if(matchArr5)matchmes += (matchmes != "" ? "," : "") + matchArr5.toString();
+
+      if(!retFlg)mes = "機種依存文字(" +matchmes+ ")が含まれています。可能な限り代替できる文字に置き換えてださい。";
+      return mes;
     }
   };
 
