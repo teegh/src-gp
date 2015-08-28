163534e71307d031050ca6439ee1a9131a19c26d
test <test@test.jp>
2015-08-28 17:19:06 +0900

    comment off

--- coffee/node-script/Utility/_RepStrUtil.js
+++ coffee/node-script/Utility/_RepStrUtil.js
@@ -47,7 +47,49 @@ var _RepStrUtil = (function() {
                 zeroString += "0";
             }
             return (zeroString + String(inNum)).slice(-1*inDigitsNum);
+        },
+
+        //16進数のカラーコードをrgb(##, ##, ##)の形式に変換する。
+        //http://php.o0o0.jp/article/jquery-rgb_color
+        Rep_HEXColorToRGBColor : function (inStr){
+
+            if(inStr && inStr != ""){
+
+                if (inStr.length == 3+1) {
+                  // #abcをa,b,cに分割
+                  var hex_3digit = inStr.match(/^#([0-9a-fA-F]{1})([0-9a-fA-F]{1})([0-9a-fA-F]{1})/);
+                  // aa
+                  var hex_r = hex_3digit[1] + hex_3digit[1];
+                  // bb
+                  var hex_g = hex_3digit[2] + hex_3digit[2];
+                  // cc
+                  var hex_b = hex_3digit[3] + hex_3digit[3];
+                } else if(inStr.length == 6+1){
+                  // #abcdefをab,cd,efに分割
+                  var hex_6digit = inStr.match(/^#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})/);
+                  // console.log( inStr+"->" + hex_6digit);
+                  // ab
+                  var hex_r = hex_6digit[1];
+                  // cd
+                  var hex_g = hex_6digit[2];
+                  // ef
+                  var hex_b = hex_6digit[3];
+                }else{
+                    return null;
+                }
+
+                // 16進数から10進数へ
+                var rgb_r = parseInt(hex_r, 16);
+                var rgb_g = parseInt(hex_g, 16);
+                var rgb_b = parseInt(hex_b, 16);
+
+                return "rgb("+ rgb_r+", "+rgb_g +", "+ rgb_b +")";
+            }else{
+                return null;
+            }
         }
+
+
     };
 
     //---------------------------
