2a19e809d3f168e8acdb3cff87bf861bf7be5293
gusen <gamagori-usen@gamagori-usen-no-iMac.local>
2015-10-30 11:43:08 +0900

    add log date

--- coffee/node-script/Utility/_Log.js
+++ coffee/node-script/Utility/_Log.js
@@ -4,6 +4,7 @@
 var _Log = (function(){//jquery closure
   
   var _filterModuleName = []; //ログとして表示するモジュール名
+  var _isVisibleDate = true;  //ログに日付を表示
   
   //get color
   function gC(inColorName){
@@ -28,7 +29,33 @@ var _Log = (function(){//jquery closure
     }
   }
   
-  
+  //日時を取得する
+  function getDateTimeStr(){
+    var weeks = new Array('日','月','火','水','木','金','土');
+    var now = new Date();
+
+    var year = now.getYear(); // 年
+    var month = now.getMonth() + 1; // 月
+    var day = now.getDate(); // 日
+    var week = weeks[ now.getDay() ]; // 曜日
+    var hour = now.getHours(); // 時
+    var min = now.getMinutes(); // 分
+    var sec = now.getSeconds(); // 秒
+
+    if(year < 2000) { year += 1900; }
+
+    // 数値が1桁の場合、頭に0を付けて2桁で表示する指定
+    if(month < 10) { month = "0" + month; }
+    if(day < 10) { day = "0" + day; }
+    if(hour < 10) { hour = "0" + hour; }
+    if(min < 10) { min = "0" + min; }
+    if(sec < 10) { sec = "0" + sec; }
+
+    // 表示開始
+    // document.write('現在：<strong>' + year + '年' + month + '月' + day + '日（' + week + '）');
+    // document.write(hour + '時' + min + '分' + sec + '秒</strong>');
+    return year+"/"+month+"/"+day+" "+hour+":"+min+":"+sec;
+  }
   
   return {
     log : function (inModuleName, isError , title , message){
@@ -48,11 +75,12 @@ var _Log = (function(){//jquery closure
         if(consoleVidible){
           var m = _RepStrUtil.Rep_SpacePaddingString(inModuleName , 16);
           var t = _RepStrUtil.Rep_SpacePaddingString(title , 16);
+          var dStr = _isVisibleDate ? getDateTimeStr() : "";
           
           if(!isError){
-            console.log(" " + gC("cyan")+ m +gC() + gC("white") + t + gC() + message);
+            console.log(" " + dStr + " " + gC("cyan") + m +gC() + gC("white") + t + gC() + message);
           }else{
-            console.error(" " + gC("cyan")+ m + gC() + gC("white")+ t + gC()  + gC("red")+message + gC());
+            console.error(" " + gC("red") +dStr +" "+ gC("cyan")+ m + gC() + gC("white")+ t + gC()  + gC("red")+message + gC());
           }
         }
         
@@ -61,6 +89,13 @@ var _Log = (function(){//jquery closure
     //ログとして表示したいモジュール名を指定する。例 ["module",...]
     setFilterModuleName : function(inFilterArr){
       _filterModuleName = inFilterArr;
+    },
+    
+    //ログに日付を表示
+    isVisibleDate : function (inFlg){
+      _isVisibleDate = inFlg;
     }
+    
+    
   };
 })();//jQuery Closure
