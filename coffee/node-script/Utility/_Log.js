16e70b83c95fd774d44a550cedb25f4d45aa0d17
gusen <gamagori-usen@gamagori-usen-no-iMac.local>
2015-10-30 09:30:28 +0900

    log color added

--- coffee/node-script/Utility/_Log.js
+++ coffee/node-script/Utility/_Log.js
@@ -3,6 +3,8 @@
 
 var _Log = (function(){//jquery closure
   
+  var _filterModuleName = []; //ログとして表示するモジュール名
+  
   //get color
   function gC(inColorName){
     if(inColorName == "black"){
@@ -27,20 +29,38 @@ var _Log = (function(){//jquery closure
   }
   
   
+  
   return {
     log : function (inModuleName, isError , title , message){
         // var em = isError ? "Error " : "";
         
-        var m = _RepStrUtil.Rep_SpacePaddingString(inModuleName , 16);
-        var t = _RepStrUtil.Rep_SpacePaddingString(title , 16);
-        
-        
-        if(!isError){
-          console.log(" " + gC("cyan")+ m +gC() + gC("white") + t + gC() + message);
-        }else{
-          console.error(" " + gC("cyan")+ m + gC() + gC("white")+ t + gC()  + gC("red")+message + gC());
+        var consoleVidible = true;
+        if(_filterModuleName && _filterModuleName.length > 0){
+          consoleVidible = false;
+          for(var i=0; i<_filterModuleName.length; i++){
+            if(_filterModuleName[i] == inModuleName){
+              consoleVidible = true;
+              break;
+            }
+          }
         }
         
+        if(consoleVidible){
+          var m = _RepStrUtil.Rep_SpacePaddingString(inModuleName , 16);
+          var t = _RepStrUtil.Rep_SpacePaddingString(title , 16);
+          
+          if(!isError){
+            console.log(" " + gC("cyan")+ m +gC() + gC("white") + t + gC() + message);
+          }else{
+            console.error(" " + gC("cyan")+ m + gC() + gC("white")+ t + gC()  + gC("red")+message + gC());
+          }
+        }
+        
+    },
+    
+    //ログとして表示したいモジュール名を指定する。例 ["module",...]
+    setFilterModuleName : function(inFilterArr){
+      _filterModuleName = inFilterArr;
     }
   };
 })();//jQuery Closure
